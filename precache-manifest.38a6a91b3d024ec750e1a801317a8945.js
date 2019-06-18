0dc10e750c734e5f4113657ed1761b3bf8e2ec09
刘进行 <jackingstarliu@gmail.com>
2019-06-18 11:18:11 +0800

    Updates

---
+++ precache-manifest.38a6a91b3d024ec750e1a801317a8945.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86350637839a38e4ce6ff2834e7f937f",
+    "url": "/todo-list-redux/index.html"
+  },
+  {
+    "revision": "6b1cdbb1d1384714c2cd",
+    "url": "/todo-list-redux/static/css/main.ef747068.chunk.css"
+  },
+  {
+    "revision": "a6aa128e5474bb41c1f0",
+    "url": "/todo-list-redux/static/js/2.691ad792.chunk.js"
+  },
+  {
+    "revision": "6b1cdbb1d1384714c2cd",
+    "url": "/todo-list-redux/static/js/main.0d1b0dfa.chunk.js"
+  },
+  {
+    "revision": "3a967740a3eb25858bc2",
+    "url": "/todo-list-redux/static/js/runtime~main.a8ec9507.js"
+  }
+]);
